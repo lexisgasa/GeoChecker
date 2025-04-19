2772b375f1174b5eea12c628dd703161a0387bbb
Alexis Garcia Saez <lexisgasa@uoc.edu>
2025-04-19 11:47:21 +0200

    Create WorkerPage component

--- frontend/src/components/router/router.tsx
+++ frontend/src/components/router/router.tsx
@@ -7,12 +7,14 @@ import App from "../App/App";
 import RoleSelectionPage from "../../geoChecker/pages/RoleSelectionPage/RoleSelectionPage";
 import NotFound from "../pages/NotFound/NotFound";
 import LoginPage from "../../geoChecker/pages/LoginPage/LoginPage";
+import WorkerPage from "../../geoChecker/pages/WorkerPage/WorkerPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<RoleSelectionPage />} />
       <Route path="login" element={<LoginPage />} />
+      <Route path="worker" element={<WorkerPage />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
