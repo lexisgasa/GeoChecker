4def5c0fe10cb11ebc52acdf12292eeee6153e6d
Alexis Garcia Saez <lexisgasa@uoc.edu>
2025-04-28 23:10:51 +0200

    Create DetailHistoryForm component

--- frontend/src/geoChecker/pages/DetailHistory/DetailHistory.tsx
+++ frontend/src/geoChecker/pages/DetailHistory/DetailHistory.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReturnButton from "../../../components/Buttons/ReturnButton";
 import "./DetailHistory.css";
+import DetailHistoryForm from "../../components/DetailHistoryForm/DetailHistoryForm";
 
 const DetailHistory: React.FC = () => {
   return (
@@ -8,23 +8,7 @@ const DetailHistory: React.FC = () => {
       <h2>Detalle de registro</h2>
       <div className="general-form">
         <p>Selecciona la fecha que deseas consultar</p>
-        <form className="form-group">
-          <div className="button-group">
-            <label htmlFor="start-date">Desde</label>
-            <input type="date" id="start-date" />
-          </div>
-          <div className="button-group">
-            <label htmlFor="end-date">Hasta</label>
-            <input type="date" id="end-date" />
-          </div>
-
-          <div className="button-group">
-            <button type="submit" className="action-button">
-              Visualizar
-            </button>
-            <ReturnButton />
-          </div>
-        </form>
+        <DetailHistoryForm />
       </div>
     </>
   );
