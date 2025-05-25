e25fc06c3e939f4ce7f4751fa45b6fbf17d72dea
Alexis Garcia Saez <lexisgasa@uoc.edu>
2025-05-25 23:05:09 +0200

    Create server error and the not found endpoint error

--- backend/src/server/app.ts
+++ backend/src/server/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import healthCheck from "./healtCheck/healthCheck.js";
+import notFoundError from "./error/notFoundError/notFoundError.js";
 
 const app = express();
 
 app.get("/", healthCheck)
+app.use(notFoundError)
 
 export default app;
---
+++ backend/src/server/error/notFoundError/NotFoundError.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import { ServerError } from "../serverError.js";
+
+const notFoundError = (_req: Request, _res: Response, next: NextFunction):void => {
+    const error = new ServerError("Endpoint not found", 404);
+
+    next(error);
+}
+
+export default notFoundError;
