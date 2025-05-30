677f314460ad4d244ba6c3ad1d76139aca750fca
Alexis Garcia Saez <lexisgasa@uoc.edu>
2025-05-30 16:35:58 +0200

    Create the function to connect to the database and setup server initialization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,20 @@
+import connectToDatabase from "./database/index.js";
 import startServer from "./server/startServer.js";
+import chalk from 'chalk';
 
-startServer(Number(process.env.PORT));
+const port = process.env.PORT || 3500;
+const databaseUrl = process.env.MONGODB_URL
+
+
+if (!databaseUrl) {
+    console.log(chalk.red("Error: missing MongoDB URL"));
+    process.exit(1)
+}
+
+try {
+    await connectToDatabase(databaseUrl);
+
+    startServer(Number(port));
+} catch (error){
+    console.log(chalk.redBright(`There was an error: ${error.message}`))
+}
