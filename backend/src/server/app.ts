f07ecd83cb2b6cf0a003e24c02f8c78744d1796a
Alexis Garcia Saez <lexisgasa@uoc.edu>
2025-06-01 00:28:04 +0200

    Rename notFoundError to its correct name

--- backend/src/server/app.ts
+++ backend/src/server/app.ts
@@ -2,13 +2,13 @@ import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import healthCheck from "./healtCheck/healthCheck.js";
-import notFoundError from "./error/_tmp_notfounderror/notFoundError.js";
 import generalError from "./error/generalError/generalError.js";
 import { v4 as uuidv4 } from 'uuid';
 import User from "../geoChecker/model/User.js";
 import authRoutes from "../geoChecker/router/router.js";
 import bcrypt from "bcrypt";
 import { authenticate, isAdmin } from "../middleware/auth.js";
+import notFoundError from "./error/notFoundError/notFoundError.js";
 
 
 const app = express();
